de6d3cba788e82e2db80c10b94e81f697199502b
ivona-samarzija <ivona.vlahov@hotmail.com>
2022-07-20 14:52:32 +0200

    fix error

--- src/components/services.vue
+++ src/components/services.vue
@@ -11,7 +11,7 @@
 
         
 
-        <img class="image" src="../assets/massage.webp">
+        <img class="image" src="../assets/massage.jpg">
         
         <div>
             <p class="massage">Masaže</p>
@@ -45,7 +45,7 @@
                 <li>Farbanje obrva</li>
             </ul>
         </div>
-        <img class="bigpic" src="../assets/facial.webp">
+        <img class="bigpic" src="../assets/facial.jpg">
     </div>
 
     <h3>Hvala vam na povjerenju!</h3>
